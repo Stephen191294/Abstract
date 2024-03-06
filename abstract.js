c17a13c5a026e2cca598ba5e57ffbafd1184f824
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-03-06 12:26:55 -0300

    project finished

--- abstract.js
+++ abstract.js
@@ -14,9 +14,52 @@ function toggleMenu() {
 }
 
 
-document.getElementById('menu-icon').addEventListener('click', function() {
+document.getElementById('menuIcon').addEventListener('click', function() {
   // Toggle visibility of menu icon and close icon
-  document.getElementById('menu-icon').style.display = 'none';
-  document.getElementById('close-icon').style.display = 'inline';
+  if(slideMenu.style.display === 'none'){
+    slideMenu.style.display = 'block';
+  }
+  else{
+    slideMenu.style.display = 'none';
+  }
+});
+
+
+
+let myIcon = document.getElementById('searchIcon');
+let secondNav = document.getElementById('secondNav');
+let mainNav = document.getElementById('mainNav');
+let secondNavInput = document.getElementById('secondNavInput');
+let closeButton = document.getElementById('closeButton');
+
+
+
+myIcon.addEventListener('click', function() {
+  if (window.matchMedia('(max-width: 800px)').matches){
+   mainNav.style.display ='none';
+    secondNav.style.display = 'block';
+    secondNavInput.style.display = 'block';
+    closeButton.style.display = 'block';
+
+  }
+  else{
+    mainNav.style.display ='block';
+    secondNavInput.style.display = 'none';
+    secondNav.style.display = 'none';
+  }
+
+});
+
+
+//close the second nav to show the main nav
+let closebutton = document.getElementById('closeButton');
+let secondnav = document.getElementById('secondNav');
+let mainnav = document.getElementById('mainNav');
+
+
+closeButton.addEventListener('click', function() {
+  // Hide the secondNav when the closeButton is clicked
+  secondNav.style.display = 'none';
+  mainNav.style.display = 'block';
 });
 
